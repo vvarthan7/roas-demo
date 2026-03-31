912ff8273020deaf52b7e327e0cfd255a5ce890c
VISHNUVARTHAN U <vvarthan7@gmail.com>
2026-03-31 07:41:51 +0530

    Implementing GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,6 +22,9 @@ export default function RootLayout({
     <html lang="en" className={`${inter.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         {children}
+        <GoogleAnalytics
+          gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ""}
+        />
         <Script
           id="meta-pixel"
           strategy="afterInteractive"
