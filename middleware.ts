fb78892c4d8698e91842cf34f70f8f1ff59ab5a6
VISHNUVARTHAN U <vvarthan7@gmail.com>
2026-04-02 11:20:52 +0530

    Updating image url

--- middleware.ts
+++ middleware.ts
@@ -59,5 +59,7 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:webp|png|jpg|jpeg|gif|svg|ico|woff|woff2|ttf|otf)$).*)",
+  ],
 };
